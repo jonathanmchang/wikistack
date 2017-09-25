add9e0a7fe35609c4ae10f90a762488c5b7969f5
Yuchen Chen <yuchchen1314@gmail.com>
2017-09-25 11:35:14 -0400

    no complete

--- app.js
+++ app.js
@@ -3,23 +3,26 @@ const app = express();
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 const chalk=require('chalk');
-
-
+const models = require('./models');
 
 app.set('view engine', 'html')
 app.engine('html', nunjucks.render);
 nunjucks.configure('views', { noCache: true });
-
 app.use(morgan('dev'));
-
 app.use(express.static(__dirname + '/public'))
 
+// app.get('/',function(req,res){
+//     res.send("you got the root")
+// })
+
 app.get('/',function(req,res){
-    res.send("you got the root")
+ res.send('./models')
 })
 
-app.listen(3000,function(){
-    console.log( "Example app listening on port 3000" )
+models.db.sync({force: true})
+.then(function () {
+    app.listen(3000, function () {
+        console.log('Server is listening on port 3000!');
+    });
 })
-
-// app.use('/', routes)
+.catch(console.error);
--- models/index.js
+++ models/index.js
@@ -1,9 +1,55 @@
 var Sequelize = require('sequelize');
-var db = new Sequelize('postgres://localhost:5432/wikistack');
+var db = new Sequelize('postgres://localhost:5432/wikistack', {
+  logging: false
+});
 
-const Page = sequelize.create('page', {
-    title: Sequelize.STRING,
-    urlTitle: Sequelize.TEXT
-    content:
+const Page =  db.define('page', {
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    urlTitle: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    getRoute: function(){
+      return '/wiki/'+this.urlTitle
+    },
+    content: {
+       type: Sequelize.TEXT,
+       allowNull: true
+    },
+    status:{
+      type: Sequelize.ENUM('open','closed'),
+      allowNull: true
+    },
+    date: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW
+    }
   })
-  
+
+const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email:{
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate:{
+      isEmail: true
+    }
+  }
+})
+
+Page.sync()
+User.sync()
+
+module.exports = {
+  Page: Page,
+  User: User,
+  db:db
+};
+
